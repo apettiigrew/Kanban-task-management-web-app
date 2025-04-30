c116366ea186ce0880083443462f569b25cde6c7
Andrew Pettigrew <devdrew10@gmail.com>
2025-04-29 21:38:45 -0400

    feat: add env var for database

--- src/lib/database-postgresql.ts
+++ src/lib/database-postgresql.ts
@@ -1,11 +1,17 @@
 import { Pool } from 'pg';
 
+if (!process.env.POSTGRES_USER) throw new Error('POSTGRES_USER is not defined');
+if (!process.env.POSTGRES_HOST) throw new Error('POSTGRES_HOST is not defined');
+if (!process.env.POSTGRES_DATABASE) throw new Error('POSTGRES_DATABASE is not defined');
+if (!process.env.POSTGRES_PASSWORD) throw new Error('POSTGRES_PASSWORD is not defined');
+if (!process.env.POSTGRES_PORT) throw new Error('POSTGRES_PORT is not defined');
+
 const pool = new Pool({
-  user: 'myuser',
-  host: 'localhost',
-  database: 'mydatabase',
-  password: 'mypassword',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: parseInt(process.env.POSTGRES_PORT),
 });
 
-export default pool; 
+export default pool;
