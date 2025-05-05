a487007146466228b7aaee9f7e85457f95451b07
Andrew Pettigrew <devdrew10@gmail.com>
2025-05-05 12:40:34 -0400

    feat: setup sass base

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import path from 'path'
 
+
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
-import './globals.scss';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from "next/font/google";
 import "./../styles/main.scss";
 
 export const metadata: Metadata = {
