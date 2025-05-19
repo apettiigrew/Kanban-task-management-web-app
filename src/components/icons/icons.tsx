97de31667ed088c6e572a9cd9240664f0df0fb95
Andrew Pettigrew <devdrew10@gmail.com>
2025-05-19 16:05:17 -0400

    feat: add dropdown menu icon

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -12,6 +12,21 @@ export const CloseIcon = () => (
   </svg>
 );
 
+
+export const MenuIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="#1f1f1f"
+    viewBox="0 -960 960 960"
+    // {...props}
+  >
+    <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+  </svg>
+)
+
+
 export const AddIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
