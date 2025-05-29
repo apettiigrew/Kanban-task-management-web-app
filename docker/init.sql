a9a04b93152b7588f3086bff05fc71b6143dc067
Andrew Pettigrew <devdrew10@gmail.com>
2025-05-28 20:54:58 -0400

    feat: wip

--- docker/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "projects" (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    deleted_at TIMESTAMP WITH TIME ZONE
-);
