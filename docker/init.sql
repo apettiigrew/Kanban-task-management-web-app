fbb03e0f7744eb8afe21dc62118c3f668594f0b2
Andrew Pettigrew <devdrew10@gmail.com>
2025-05-30 13:31:34 -0400

    feat: add docker compose file

---
+++ docker/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "projects" (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE
+);
